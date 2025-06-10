d67eb03d1feb0dfc1e0a2b4014f051660f4271d6
wenhao999999999999 <zhangwenhao_9@163.com>
2025-06-10 21:54:02 +0800

    力扣添加组合排列元素可重复题解

--- LeetCode/allPermutation.cpp
+++ LeetCode/allPermutation.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 class Solution {
-public:
+private:
     vector<vector<int>> result;
     vector<int> path;
     vector<bool> used;
-
+public:
     void backtracking(vector<int>& nums) {
 
         if (path.size() == nums.size()) {
@@ -29,13 +29,53 @@ public:
         backtracking(nums);
         return result;
     }
+
+    void backtrackingUnique(vector<int>& nums) {
+
+        //base case
+        if (path.size() == nums.size()) {
+            result.push_back(path);
+            return;
+        }
+
+        int sz = nums.size();
+        for (int i = 0; i < sz; i++) {
+            if (used[i]) continue;
+            //剪枝，保证相邻相等元素的相对顺序
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) {
+                continue;
+            }
+
+            //choose
+            path.push_back(nums[i]);
+            used[i] = true;
+
+            //recursion
+            backtrackingUnique(nums);
+
+            //撤销选择
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        used.resize(nums.size(), false);
+        backtrackingUnique(nums);
+        return result;
+    }
 };
+
+
 
 int main() {
     vector<int> nums = {1, 2, 3};
+    vector<int> nums2 = {1, 2, 2};
     Solution sol;
-    vector<vector<int>> res = sol.allPermute(nums);
-    for (auto& vec : res) {
+    // vector<vector<int>> res = sol.allPermute(nums);
+    vector<vector<int>> res2 = sol.permuteUnique(nums2);
+    for (auto& vec : res2) {
         for (auto& num : vec) {
             cout << num << " ";
         }
--- LeetCode/comBine.cpp
+++ LeetCode/comBine.cpp
@@ -6,8 +6,14 @@ public:
     vector<vector<int>> result;
     vector<int> track;
 
+    //用于记录当前路径的元素和
+    int tracksum = 0;
+
+
+    //目标个数的组合
     void backtracking(int n, int k, int start) {
         int sz = track.size();
+        //base case 
         if (sz == k) {
             result.push_back(track);
             //提前终止，避免冗余计算
@@ -15,8 +21,11 @@ public:
         }
 
         for(int i = start; i <= n; i++){
+            //选择
             track.push_back(i);
+            //递归
             backtracking(n, k, i+1);
+            //撤销选择
             track.pop_back();
         }
     }
@@ -25,12 +34,88 @@ public:
         backtracking(n, k, 1);
         return result;
     }
+
+    //目标和的组合,不可重复
+    void backtracking2(int target, int start, vector<int>& candidates) {
+        //base case
+        if (tracksum == target) {
+            result.push_back(track);
+            return; 
+        }
+
+        if (tracksum > target) {
+            return;
+        }
+
+        int sz = candidates.size();
+        for(int i = start; i < sz; i++) {
+            //跳过相同项
+            if (i > start && candidates[i] == candidates[i-1]) {
+                continue;
+            }
+            //选择
+            track.push_back(candidates[i]);
+            tracksum += candidates[i];
+            //递归
+            backtracking2(target, i+1, candidates);
+
+            //撤销选择
+            track.pop_back();
+            tracksum -= candidates[i];
+        }
+    }
+
+    vector<vector<int>> combineSum(int target, vector<int>& candidates) {
+        //排序，使得相同元素相邻,用于处理输入数组中含相同元素的情况
+        sort(candidates.begin(), candidates.end());
+        backtracking2(target, 0, candidates);
+        return result;
+
+    }
+
+    //输入数组无重复，元素可重复使用
+    void backtrackint3(int start, int target, vector<int> nums) {
+        //base case
+        if (tracksum == target) {
+            result.push_back(track);
+            return;
+        }
+
+        if (tracksum > target) {
+            return;
+        }
+        int sz = nums.size();
+        for (int i = start; i < sz; i++){
+            //做选择
+            track.push_back(nums[i]);
+            tracksum += nums[i];
+
+            //递归
+            backtrackint3(i, target, nums);
+
+            //撤销选择
+            track.pop_back();
+            tracksum -= nums[i];
+        }
+        
+        
+        
+    }
+
+    vector<vector<int>> combineSum2(vector<int>& candidates, int target) {
+        backtrackint3(0, target, candidates);
+        return result;
+    }
+
 };
 
 int main() {
     Solution s;
-    vector<vector<int>> result = s.combine(4, 2);
-    for (auto vec : result) {
+    // vector<vector<int>> result = s.combine(4, 2);
+    // vector<int> candidates = {10,1,2,7,6,1,5};
+    vector<int> candidates = {2,3,6,7};
+    vector<vector<int>> result2 = s.combineSum2(candidates, 7);
+    for (auto vec : result2) {
         for (int num : vec) {            
             cout << num << " ";
         }
